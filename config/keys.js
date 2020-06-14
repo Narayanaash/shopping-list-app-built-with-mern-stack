0c771f31ec553ae2e25ddbee78144199734277fd
!_Narayan_! <32462580+Narayanaash@users.noreply.github.com>
2020-06-14 23:30:31 +0530

    Update keys.js

--- config/keys.js
+++ config/keys.js
@@ -1,3 +1,3 @@
 module.exports = {
-    mongoURI: 'mongodb://narayan:narayan123@ds053216.mlab.com:53216/shopping_list'
+    mongoURI: 'mongodb://<yourusername>:<yourpassword>@ds053216.mlab.com:53216/shopping_list'
 }
